e17b749bb8ca08d04cc61323a90df8a00919b019
zas <zas@mail.ustc.edu.cn>
2018-09-01 18:19:45 +0800

    对二值化后的图像做膨胀处理后，再利用ZhangSuen做细化

--- zhouanshun/zhouanshun/image_thinning.cpp
+++ zhouanshun/zhouanshun/image_thinning.cpp
@@ -1,22 +1,167 @@
-/*测试程序-加载、显示、修改、保存*/
+#include <iostream>
 #include <opencv2/opencv.hpp>
-#include <iostream> 
-#include <math.h>
+using namespace std;
 using namespace cv;
-using namespace std; //使用命名空间std，可以调用C++标准库里面的函数或类 
-int main(int argc, char** argv) {    //argc 表示命令行输入参数的个数（以空白符分隔）， argv中存储了所有的命令行参数
-	Mat src = imread("D:/path_pic/left.jpg");     //载入图像
-	if (src.empty()) {
-		printf("could not load image...\n");
-		return -1;
+
+void thinImage(Mat & srcImg) {
+	vector<Point> deleteList;
+	int neighbourhood[9];
+	int nl = srcImg.rows;
+	int nc = srcImg.cols;
+	bool inOddIterations = true;
+	while (true) {
+		for (int j = 1; j < (nl - 1); j++) {
+			uchar* data_last = srcImg.ptr<uchar>(j - 1);
+			uchar* data = srcImg.ptr<uchar>(j);
+			uchar* data_next = srcImg.ptr<uchar>(j + 1);
+			for (int i = 1; i < (nc - 1); i++) {
+				if (data[i] == 255) {
+					int whitePointCount = 0;
+					neighbourhood[0] = 1;
+					if (data_last[i] == 255) neighbourhood[1] = 1;
+					else  neighbourhood[1] = 0;
+					if (data_last[i + 1] == 255) neighbourhood[2] = 1;
+					else  neighbourhood[2] = 0;
+					if (data[i + 1] == 255) neighbourhood[3] = 1;
+					else  neighbourhood[3] = 0;
+					if (data_next[i + 1] == 255) neighbourhood[4] = 1;
+					else  neighbourhood[4] = 0;
+					if (data_next[i] == 255) neighbourhood[5] = 1;
+					else  neighbourhood[5] = 0;
+					if (data_next[i - 1] == 255) neighbourhood[6] = 1;
+					else  neighbourhood[6] = 0;
+					if (data[i - 1] == 255) neighbourhood[7] = 1;
+					else  neighbourhood[7] = 0;
+					if (data_last[i - 1] == 255) neighbourhood[8] = 1;
+					else  neighbourhood[8] = 0;
+					for (int k = 1; k < 9; k++) {
+						whitePointCount += neighbourhood[k];
+					}
+					if ((whitePointCount >= 2) && (whitePointCount <= 6)) {
+						int ap = 0;
+						if ((neighbourhood[1] == 0) && (neighbourhood[2] == 1)) ap++;
+						if ((neighbourhood[2] == 0) && (neighbourhood[3] == 1)) ap++;
+						if ((neighbourhood[3] == 0) && (neighbourhood[4] == 1)) ap++;
+						if ((neighbourhood[4] == 0) && (neighbourhood[5] == 1)) ap++;
+						if ((neighbourhood[5] == 0) && (neighbourhood[6] == 1)) ap++;
+						if ((neighbourhood[6] == 0) && (neighbourhood[7] == 1)) ap++;
+						if ((neighbourhood[7] == 0) && (neighbourhood[8] == 1)) ap++;
+						if ((neighbourhood[8] == 0) && (neighbourhood[1] == 1)) ap++;
+						if (ap == 1) {
+							if (inOddIterations && (neighbourhood[3] * neighbourhood[5] * neighbourhood[7] == 0)
+								&& (neighbourhood[1] * neighbourhood[3] * neighbourhood[5] == 0)) {
+								deleteList.push_back(Point(i, j));
+							}
+							else if (!inOddIterations && (neighbourhood[1] * neighbourhood[5] * neighbourhood[7] == 0)
+								&& (neighbourhood[1] * neighbourhood[3] * neighbourhood[7] == 0)) {
+								deleteList.push_back(Point(i, j));
+							}
+						}
+					}
+				}
+			}
+		}
+		if (deleteList.size() == 0)
+			break;
+		for (size_t i = 0; i < deleteList.size(); i++) {
+			Point tem;
+			tem = deleteList[i];
+			uchar* data = srcImg.ptr<uchar>(tem.y);
+			data[tem.x] = 0;
+		}
+		deleteList.clear();
+
+		inOddIterations = !inOddIterations;
 	}
-	namedWindow("test opencv setup", WINDOW_AUTOSIZE);   //创建窗口，自动大小
-	imshow("test opencv setup", src);               //显示图像到指定的窗口
-	namedWindow("output windows", CV_WINDOW_AUTOSIZE);
-	Mat output_image;
-	cvtColor(src, output_image, CV_BGR2HLS);     //修改图像为某色彩空间
-	imshow("output windows", output_image);
-	imwrite("D:/path_pic/my.tif", output_image);   //重命名为my.tif并保存到D盘
+}
+
+//求九个数的中值
+uchar Median(uchar n1, uchar n2, uchar n3, uchar n4, uchar n5,
+	uchar n6, uchar n7, uchar n8, uchar n9) {
+	uchar arr[9];
+	arr[0] = n1;
+	arr[1] = n2;
+	arr[2] = n3;
+	arr[3] = n4;
+	arr[4] = n5;
+	arr[5] = n6;
+	arr[6] = n7;
+	arr[7] = n8;
+	arr[8] = n9;
+	for (int gap = 9 / 2; gap > 0; gap /= 2)//希尔排序
+		for (int i = gap; i < 9; ++i)
+			for (int j = i - gap; j >= 0 && arr[j] > arr[j + gap]; j -= gap)
+				swap(arr[j], arr[j + gap]);
+	return arr[4];//返回中值
+}
+
+//中值滤波
+void MedianFlitering(const Mat &src, Mat &dst) {
+	if (!src.data)return;
+	Mat _dst(src.size(), src.type());
+	for (int i = 0; i < src.rows; ++i)
+		for (int j = 0; j < src.cols; ++j) {
+			if ((i - 1) > 0 && (i + 1) < src.rows && (j - 1) > 0 && (j + 1) < src.cols) {
+				_dst.at<Vec3b>(i, j)[0] = Median(src.at<Vec3b>(i, j)[0], src.at<Vec3b>(i + 1, j + 1)[0],
+					src.at<Vec3b>(i + 1, j)[0], src.at<Vec3b>(i, j + 1)[0], src.at<Vec3b>(i + 1, j - 1)[0],
+					src.at<Vec3b>(i - 1, j + 1)[0], src.at<Vec3b>(i - 1, j)[0], src.at<Vec3b>(i, j - 1)[0],
+					src.at<Vec3b>(i - 1, j - 1)[0]);
+				_dst.at<Vec3b>(i, j)[1] = Median(src.at<Vec3b>(i, j)[1], src.at<Vec3b>(i + 1, j + 1)[1],
+					src.at<Vec3b>(i + 1, j)[1], src.at<Vec3b>(i, j + 1)[1], src.at<Vec3b>(i + 1, j - 1)[1],
+					src.at<Vec3b>(i - 1, j + 1)[1], src.at<Vec3b>(i - 1, j)[1], src.at<Vec3b>(i, j - 1)[1],
+					src.at<Vec3b>(i - 1, j - 1)[1]);
+				_dst.at<Vec3b>(i, j)[2] = Median(src.at<Vec3b>(i, j)[2], src.at<Vec3b>(i + 1, j + 1)[2],
+					src.at<Vec3b>(i + 1, j)[2], src.at<Vec3b>(i, j + 1)[2], src.at<Vec3b>(i + 1, j - 1)[2],
+					src.at<Vec3b>(i - 1, j + 1)[2], src.at<Vec3b>(i - 1, j)[2], src.at<Vec3b>(i, j - 1)[2],
+					src.at<Vec3b>(i - 1, j - 1)[2]);
+			}
+			else
+				_dst.at<Vec3b>(i, j) = src.at<Vec3b>(i, j);
+		}
+	_dst.copyTo(dst);//拷贝
+}
+
+
+int main(int argc, char* argv[])
+{
+	// image path from command argv
+	//Mat img = imread(argv[1], -1);
+	String imagePath("../../shared_data/stm32/path1.jpg");
+	Mat img = imread(imagePath, -1);
+	if (img.empty())
+	{
+		cout << "Error: Could not load image" << endl;
+		return 0;
+	}
+
+	Mat gray;
+	cvtColor(img, gray, CV_BGR2GRAY);
+
+	//Mat smooth;
+	//MedianFlitering(img,smooth);//中值滤波意义不大
+
+	Mat dst;
+	threshold(gray, dst, 0, 255, CV_THRESH_OTSU);
+
+	Mat dilateion;
+	dilate(dst, dilateion, Mat(),Point(),5);
+
+	Mat line;
+	line = dilateion.clone();   //克隆二值化图像
+	thinImage(line);
+
+	Mat line_2;
+	line_2 = img.clone();
+	thinImage(line_2);
+
+	imshow("src", img);
+	imshow("gray", gray);
+	imshow("dst", dst);
+	imshow("dilateion", dilateion);
+	//imshow("smooth", smooth);
+	imshow("line", line);
 	waitKey(0);
+
 	return 0;
 }
+
