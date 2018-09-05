d3247f97d8215dc13f33b558d4dd713875d8b524
zas <zas@mail.ustc.edu.cn>
2018-09-05 18:47:37 +0800

    图像预处理闭合与膨胀结果比较

--- zhouanshun/zhouanshun/image_thinning.cpp
+++ zhouanshun/zhouanshun/image_thinning.cpp
@@ -121,12 +121,69 @@ void MedianFlitering(const Mat &src, Mat &dst) {
 	_dst.copyTo(dst);//拷贝
 }
 
+//同一窗口显示多个图片 算法局限只能显示原图
+Mat combineImages(vector<Mat> imgs,//@parameter1:需要显示的图像组 
+	int col,//parameter2:显示的列数
+	int row, //parameter3:显示的行数
+	bool hasMargin) {//parameter4:是否设置边框
+	int imgAmount = imgs.size();//获取需要显示的图像数量
+	int width = imgs[0].cols;//本函数默认需要显示的图像大小相同
+	int height = imgs[0].rows;//获取图像宽高
+	int newWidth, newHeight;//新图像宽高
+	if (!hasMargin) {
+		newWidth = col * imgs[0].cols;//无边框，新图像宽/高=原图像宽/高*列/行数
+		newHeight = row * imgs[0].rows;
+	}
+	else {
+		newWidth = (col + 1) * 20 + col * width;//有边框，要将上边框的尺寸，这里设置边框为20px
+		newHeight = (row + 1) * 20 + row * height;
+	}
+
+	Mat newImage(newHeight, newWidth, CV_8UC3, Scalar(255, 255, 255));//显示创建设定尺寸的新的大图像；色深八位三通道；填充为白色
+
+
+	int x, y, imgCount;//x列号，y行号，imgCount图片序号
+	if (hasMargin) {//有边框
+		imgCount = 0;
+		x = 0; y = 0;
+		while (imgCount < imgAmount) {
+			Mat imageROI = newImage(Rect(x*width + (x + 1) * 20, y*height + (y + 1) * 20, width, height));//创建感兴趣区域
+			imgs[imgCount].copyTo(imageROI);//将图像复制到大图中
+			imgCount++;
+			if (x == (col - 1)) {
+				x = 0;
+				y++;
+			}
+			else {
+				x++;
+			}//移动行列号到下一个位置
+		}
+	}
+	else {//无边框
+		imgCount = 0;
+		x = 0; y = 0;
+		while (imgCount < imgAmount) {
+			Mat imageROI = newImage(Rect(x*width, y*height, width, height));
+			imgs[imgCount].copyTo(imageROI);
+			imgCount++;
+			if (x == (col - 1)) {
+				x = 0;
+				y++;
+			}
+			else {
+				x++;
+			}
+		}
+	}
+	return newImage;//返回新的组合图像
+};
+
 
 int main(int argc, char* argv[])
 {
 	// image path from command argv
 	//Mat img = imread(argv[1], -1);
-	String imagePath("../../shared_data/stm32/path1.jpg");
+	String imagePath("../../shared_data/stm32/path3.jpg");
 	Mat img = imread(imagePath, -1);
 	if (img.empty())
 	{
@@ -137,8 +194,8 @@ int main(int argc, char* argv[])
 	Mat gray;
 	cvtColor(img, gray, CV_BGR2GRAY);
 
-	//Mat smooth;
-	//MedianFlitering(img,smooth);//中值滤波意义不大
+	Mat smooth;
+	MedianFlitering(img,smooth);//中值滤波意义不大
 
 	Mat dst;
 	threshold(gray, dst, 0, 255, CV_THRESH_OTSU);
@@ -146,20 +203,33 @@ int main(int argc, char* argv[])
 	Mat dilateion;
 	dilate(dst, dilateion, Mat(),Point(),5);
 
+	Mat close;
+	morphologyEx(dst, close, MORPH_CLOSE, Mat(), Point(), 5);
+
 	Mat line;
 	line = dilateion.clone();   //克隆二值化图像
 	thinImage(line);
 
 	Mat line_2;
-	line_2 = img.clone();
+	line_2 = close.clone();
 	thinImage(line_2);
 
-	imshow("src", img);
+	//Mat combineImages(vector<Mat> imgs, int col, int row, bool hasMargin);//函数声明
+	//vector<Mat> imgs(2);
+	//imgs[0] = img;
+	//imgs[1] = gray;
+	//Mat m = combineImages(imgs, 1, 2, true);
+	//namedWindow("test", 0);
+	//imshow("test", m);
+
+	/*imshow("src", img);
 	imshow("gray", gray);
-	imshow("dst", dst);
+	imshow("dst", dst);*/
 	imshow("dilateion", dilateion);
 	//imshow("smooth", smooth);
 	imshow("line", line);
+	imshow("close", close);
+	imshow("line_2", line_2);
 	waitKey(0);
 
 	return 0;
