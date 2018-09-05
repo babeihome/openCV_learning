d6f2ccd1fc8eaebab08037a3ab77e0b870437a56
XuJiacong <642153483@qq.com>
2018-09-05 20:43:53 +0800

    加入霍夫变换

--- Jiacong_Xu/Project1/Project1/源.cpp
+++ Jiacong_Xu/Project1/Project1/源.cpp
@@ -14,9 +14,8 @@ int main()
 
 {
 	void chao_thinimage(Mat &srcimage);
-	Mat src, dst, src_gray,dst_processed;
-	src = imread("C:/OpenCV/opencv/straight_line.png");
-
+	Mat src, dst, src_gray,dst_processed,dst_ok, dst_Image;
+	src = imread("C:/OpenCV/straight_line.png");
 	/*if (src.data) {
 		printf("can not open\n");
 		return -1;
@@ -25,11 +24,47 @@ int main()
 	cvtColor(src, src_gray, CV_BGR2GRAY); 
 	threshold(src_gray, dst, 0, 255,  CV_THRESH_OTSU);
 	Mat element(5, 5, CV_8U, Scalar(1));
-	morphologyEx(dst, dst_processed, MORPH_OPEN, element);
-	chao_thinimage(dst_processed);
+	morphologyEx(dst, dst_processed, MORPH_OPEN, element);   //开运算
+	morphologyEx(dst_processed, dst_ok, MORPH_CLOSE, element);   //闭运算
+	chao_thinimage(dst_ok);
+
+	cvtColor(dst_ok, dst_Image, COLOR_GRAY2BGR);
+
+
+	//进行霍夫变换
+	vector<Vec2f> lines;            //定义一个矢量结构用于存放得到的线段矢量集合
+	HoughLines(dst_ok, lines, 1, CV_PI / 180, 230, 0, 0);
+
+	//在图中绘出线段
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		float rho = lines[i][0];
+		float theta = lines[i][1];
+		Point pt1, pt2;
+		double a = cos(theta), b = sin(theta);
+		double x0 = a * rho, y0 = b * rho;
+		pt1.x = cvRound(x0 + 1000 * (-b));
+		pt1.y = cvRound(y0 + 1000 * (a));
+		pt2.x = cvRound(x0 - 1000 * (-b));
+		pt2.y = cvRound(y0 - 1000 * (a));
+		line(dst_Image, pt1, pt2, Scalar(0, 0, 255), 1, LINE_AA);
+	}
+
+
+
+	//进行霍夫变换
+	//vector<Vec4i> lines;
+	//HoughLinesP(dst_ok, lines, 1, CV_PI / 180, 200, 30, );
+	//for (size_t i = 0; i < lines.size(); i++)
+	//{
+		//line(dst_Image, Point(lines[i][0], lines[i][1]), Point(lines[i][2], lines[i][3]),
+			//Scalar(0, 0, 255), 1, 8);
+	//}
+
+
 // show original image
-	namedWindow("input_image", CV_WINDOW_AUTOSIZE);
-	imshow("input_image", src);
+	//namedWindow("input_image", CV_WINDOW_AUTOSIZE);
+	//imshow("input_image", src);
 
 // show gray-level image
 	//namedWindow("output_image", CV_WINDOW_AUTOSIZE);
@@ -41,11 +76,13 @@ int main()
 
 // show smoothed image
 	namedWindow("processing_image", CV_WINDOW_AUTOSIZE);
-	imshow("processing_image", dst_processed);
+	imshow("processing_image", dst_ok);
+	namedWindow("huofu_image", CV_WINDOW_AUTOSIZE);
+	imshow("huofu_image", dst_Image);
 
 
 	waitKey(0);
-	cvDestroyWindow("input_image");
+	cvDestroyWindow("huofu_image");
 	cvDestroyWindow("processing_image");
 	return 0;
 
