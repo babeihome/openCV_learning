1e60df990c908a57472ccf9a5728a0547d840f6e
XuJiacong <642153483@qq.com>
2018-09-08 20:27:46 +0800

    霍夫变换加入

--- Testbench/Testbench/imageProcessing.cpp
+++ Testbench/Testbench/imageProcessing.cpp
@@ -44,9 +44,16 @@ void thinning(Mat &origin_img, Mat &dst_img, int method_code) {
 int analysis(Mat &origin_img, vector < pair < float, float >> &kb, int method_code) {
 	switch (method_code)
 	{
-	case 0:
+	case 0: 	
+		//进行标准霍夫变换
+		vector<Vec2f> lines;            //定义一个矢量结构用于存放得到的线段矢量集合
+		HoughLines(origin_img, lines, 1, CV_PI / 180, 230, 0, 0);
 		break;
-	default:
+	case 1:
+		//进行累计概率变换
+        vector<Vec4i> lines;
+	    HoughLinesP(origin_img, lines, 1, CV_PI / 180, 200, 30 ,10);
+	default: cout << "no this method, sorry" << endl;
 		break;
 	}
 	return 0;
