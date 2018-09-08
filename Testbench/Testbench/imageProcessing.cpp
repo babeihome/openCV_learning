442eb62974c31ab77f65973d850d11f68f5d35d2
zas <zas@mail.ustc.edu.cn>
2018-09-08 19:58:55 +0800

    合并

--- Testbench/Testbench/imageProcessing.cpp
+++ Testbench/Testbench/imageProcessing.cpp
@@ -1,3 +1,4 @@
+#include <opencv2/opencv.hpp>
 #include "imageProcessing.h"
 
 using namespace std;
@@ -60,6 +61,52 @@ int analysis(Mat &origin_img, vector < pair < float, float >> &kb, int method_co
 	return 0;
 }
 
+<<<<<<< HEAD
+//求九个数的中值
+uchar Median(uchar n1, uchar n2, uchar n3, uchar n4, uchar n5,
+	uchar n6, uchar n7, uchar n8, uchar n9) {
+	uchar arr[9];
+	arr[0] = n1;
+	arr[1] = n2;
+	arr[2] = n3;
+	arr[3] = n4;
+	arr[4] = n5;
+	arr[5] = n6;
+	arr[6] = n7;
+	arr[7] = n8;
+	arr[8] = n9;
+	for (int gap = 9 / 2; gap > 0; gap /= 2)//希尔排序
+		for (int i = gap; i < 9; ++i)
+			for (int j = i - gap; j >= 0 && arr[j] > arr[j + gap]; j -= gap)
+				swap(arr[j], arr[j + gap]);
+	return arr[4];//返回中值
+}
+
+//中值滤波
+void MedianFlitering(Mat &src, Mat &dst) {
+	if (!src.data)return;
+	Mat _dst(src.size(), src.type());
+	for (int i = 0; i < src.rows; ++i)
+		for (int j = 0; j < src.cols; ++j) {
+			if ((i - 1) > 0 && (i + 1) < src.rows && (j - 1) > 0 && (j + 1) < src.cols) {
+				_dst.at<Vec3b>(i, j)[0] = Median(src.at<Vec3b>(i, j)[0], src.at<Vec3b>(i + 1, j + 1)[0],
+					src.at<Vec3b>(i + 1, j)[0], src.at<Vec3b>(i, j + 1)[0], src.at<Vec3b>(i + 1, j - 1)[0],
+					src.at<Vec3b>(i - 1, j + 1)[0], src.at<Vec3b>(i - 1, j)[0], src.at<Vec3b>(i, j - 1)[0],
+					src.at<Vec3b>(i - 1, j - 1)[0]);
+				_dst.at<Vec3b>(i, j)[1] = Median(src.at<Vec3b>(i, j)[1], src.at<Vec3b>(i + 1, j + 1)[1],
+					src.at<Vec3b>(i + 1, j)[1], src.at<Vec3b>(i, j + 1)[1], src.at<Vec3b>(i + 1, j - 1)[1],
+					src.at<Vec3b>(i - 1, j + 1)[1], src.at<Vec3b>(i - 1, j)[1], src.at<Vec3b>(i, j - 1)[1],
+					src.at<Vec3b>(i - 1, j - 1)[1]);
+				_dst.at<Vec3b>(i, j)[2] = Median(src.at<Vec3b>(i, j)[2], src.at<Vec3b>(i + 1, j + 1)[2],
+					src.at<Vec3b>(i + 1, j)[2], src.at<Vec3b>(i, j + 1)[2], src.at<Vec3b>(i + 1, j - 1)[2],
+					src.at<Vec3b>(i - 1, j + 1)[2], src.at<Vec3b>(i - 1, j)[2], src.at<Vec3b>(i, j - 1)[2],
+					src.at<Vec3b>(i - 1, j - 1)[2]);
+			}
+			else
+				_dst.at<Vec3b>(i, j) = src.at<Vec3b>(i, j);
+		}
+	_dst.copyTo(dst);//拷贝
+=======
 void chao_thinimage(Mat &srcimage)//单通道、二值化后的图像
 
 {
@@ -327,49 +374,5 @@ void chao_thinimage(Mat &srcimage)//单通道、二值化后的图像
 		deletelist1.clear();
 
 	}
-}
-//求九个数的中值
-uchar Median(uchar n1, uchar n2, uchar n3, uchar n4, uchar n5,
-	uchar n6, uchar n7, uchar n8, uchar n9) {
-	uchar arr[9];
-	arr[0] = n1;
-	arr[1] = n2;
-	arr[2] = n3;
-	arr[3] = n4;
-	arr[4] = n5;
-	arr[5] = n6;
-	arr[6] = n7;
-	arr[7] = n8;
-	arr[8] = n9;
-	for (int gap = 9 / 2; gap > 0; gap /= 2)//希尔排序
-		for (int i = gap; i < 9; ++i)
-			for (int j = i - gap; j >= 0 && arr[j] > arr[j + gap]; j -= gap)
-				swap(arr[j], arr[j + gap]);
-	return arr[4];//返回中值
-}
-
-//中值滤波
-void MedianFlitering(Mat &src, Mat &dst) {
-	if (!src.data)return;
-	Mat _dst(src.size(), src.type());
-	for (int i = 0; i < src.rows; ++i)
-		for (int j = 0; j < src.cols; ++j) {
-			if ((i - 1) > 0 && (i + 1) < src.rows && (j - 1) > 0 && (j + 1) < src.cols) {
-				_dst.at<Vec3b>(i, j)[0] = Median(src.at<Vec3b>(i, j)[0], src.at<Vec3b>(i + 1, j + 1)[0],
-					src.at<Vec3b>(i + 1, j)[0], src.at<Vec3b>(i, j + 1)[0], src.at<Vec3b>(i + 1, j - 1)[0],
-					src.at<Vec3b>(i - 1, j + 1)[0], src.at<Vec3b>(i - 1, j)[0], src.at<Vec3b>(i, j - 1)[0],
-					src.at<Vec3b>(i - 1, j - 1)[0]);
-				_dst.at<Vec3b>(i, j)[1] = Median(src.at<Vec3b>(i, j)[1], src.at<Vec3b>(i + 1, j + 1)[1],
-					src.at<Vec3b>(i + 1, j)[1], src.at<Vec3b>(i, j + 1)[1], src.at<Vec3b>(i + 1, j - 1)[1],
-					src.at<Vec3b>(i - 1, j + 1)[1], src.at<Vec3b>(i - 1, j)[1], src.at<Vec3b>(i, j - 1)[1],
-					src.at<Vec3b>(i - 1, j - 1)[1]);
-				_dst.at<Vec3b>(i, j)[2] = Median(src.at<Vec3b>(i, j)[2], src.at<Vec3b>(i + 1, j + 1)[2],
-					src.at<Vec3b>(i + 1, j)[2], src.at<Vec3b>(i, j + 1)[2], src.at<Vec3b>(i + 1, j - 1)[2],
-					src.at<Vec3b>(i - 1, j + 1)[2], src.at<Vec3b>(i - 1, j)[2], src.at<Vec3b>(i, j - 1)[2],
-					src.at<Vec3b>(i - 1, j - 1)[2]);
-			}
-			else
-				_dst.at<Vec3b>(i, j) = src.at<Vec3b>(i, j);
-		}
-	_dst.copyTo(dst);//拷贝
+>>>>>>> f626174f6c30a160c1c6bcd3b3c94029d6a43da8
 }
